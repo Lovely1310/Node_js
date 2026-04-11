4c6ba106d32df418101a3bdccd1844499aca7df1
Monika Saini <monika9899803392@gmail.com>
2026-04-11 07:44:18 +0000

    added importExport file

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+const {add,sub,mul,div} = require('./oper');
+
+console.log(add(5,50));
+console.log(sub(50,5));
+console.log(mul(5,5));
+console.log(div(50,5));
---
+++ importExport/oper.js
@@ -0,0 +1,21 @@
+const add = (a,b)=> {
+  return a+b;
+};
+
+const sub = (a,b)=> {
+  return a-b;
+};
+
+
+const mul = (a,b)=> {
+  return a*b;
+};
+
+const div = (a,b)=> {
+  return b/a;
+};
+
+const name = "Monika";
+
+
+module.exports = {add , mul,div,sub};
