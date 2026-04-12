bdc54776bb425870985242b974c7ec7756f14856
Monika Saini <monika9899803392@gmail.com>
2026-04-12 13:18:28 +0000

    added global nodemon

--- importExport/npmMod/index.js
+++ importExport/npmMod/index.js
@@ -4,6 +4,6 @@ console.log(chalk.blue.underline.inverse("hello world"));
 console.log(chalk.keyword('red')("hello world"));
 console.log(chalk.green.underline.inverse("success"));
 
-const res = validator.isEmail("monika9899@gmail.com");
+const res = validator.isEmail("monika9899gmail.com");
 console.log(res);
 console.log(res?chalk.greenBright.inverse(res):chalk.redBright.inverse(res));
