c4f40ee85a9e10adbcbb48e28ac50ac28793c1e7
Monika Saini <monika9899803392@gmail.com>
2026-04-13 06:56:18 +0000

    Added ModWrapper Function

---
+++ ModWrapperFun/index.js
@@ -0,0 +1,17 @@
+
+
+
+const a = require('fs');
+const name = 'Monika';
+console.log(name);
+console.log(__dirname);
+console.log(__filename);
+
+
+
+// (function(){
+// var a = "lovely";
+// console.log(a);
+
+// })();
+
